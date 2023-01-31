2ec26033edc21ea7fddee6ae338256d9ed238fc4
Saki042 <90747315+Saki042@users.noreply.github.com>
2023-02-01 00:23:22 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+   public static void main(String[] args) {
+      // Prints "Hello, World in Windows Terminal for Jenkins Demo purpose."
+       System.out.println("Hello, World");
+  }
+}
